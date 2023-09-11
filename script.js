bff2c32757895ef98d0cba4e80edd5a921bf988a
Nicky <nickshah534@gmail.com>
2023-09-11 10:38:53 +1000

    Updated for . function

--- script.js
+++ script.js
@@ -14,39 +14,60 @@ const display = (str) => {
 // console.log(displayElm)
 const operators = ["+", "-", "*","/", "%"]
 
+let lastOperator = ""
+
 
 allBtns.forEach((btn) =>{
     
-btn.addEventListener("click", () => { 
-    
-    const val = btn.innerText
-
-    if (val === "AC"){
-        strToDisplay = ""
-        display(strToDisplay)
-        return
-    }
-    if (val === "C"){
-        strToDisplay= strToDisplay.slice(0, -1)
-        return display(strToDisplay)
-    }
-
-    if (val === "="){
-            const lastChar = strToDisplay[strToDisplay.length-1]
-    
-            if (operators.includes(lastChar)){
-                // removes the last character from the
-                strToDisplay= strToDisplay.slice(0, -1)
-            }
+    btn.addEventListener("click", () => { 
         
-        return total()
-    }
+        const val = btn.innerText
+
+        if (val === "AC"){
+            strToDisplay = ""
+            display(strToDisplay)
+            return
+        }
+        if (val === "C"){
+            strToDisplay= strToDisplay.slice(0, -1)
+            return display(strToDisplay)
+        }
+
+        if (val === "="){
+                const lastChar = strToDisplay[strToDisplay.length-1]
+        
+                if (operators.includes(lastChar)){
+                    // removes the last character from the
+                    strToDisplay= strToDisplay.slice(0, -1)
+                }
+            
+            return total()
+        }
 
         if (operators.includes(val)){
-        const lastChar = strToDisplay[strToDisplay.length-1]
+            lastOperator = val
+            const lastChar = strToDisplay[strToDisplay.length-1]
 
-        if (operators.includes(lastChar)){
-            strToDisplay= strToDisplay.slice(0, -1)
+            if (operators.includes(lastChar)){
+                strToDisplay= strToDisplay.slice(0, -1)
+            }
+        }
+
+    if(val === "."){
+        // if(strToDisplay.includes("."))
+        // return    //won't work cuz after operator this wont work cuz its already been used once.
+        const indexOfLastOperator = strToDisplay.lastIndexOf(lastOperator)
+
+        const lastNumberSet = strToDisplay.slice(indexOfLastOperator)
+
+        // alert(indexOfLastOperator)
+
+        if(lastNumberSet.includes(".")){
+            return
+        }
+
+        if(!lastOperator  && strToDisplay.includes(".")){
+            return
         }
     }
 
